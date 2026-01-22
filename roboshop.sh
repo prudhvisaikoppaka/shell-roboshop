fee323fb3b9613d50e92ea8cdb8922e51de69984
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-22 13:12:39 +0530

    roboshop documentation

--- roboshop.sh
+++ roboshop.sh
@@ -8,10 +8,8 @@ DOMAIN_NAME="prudhvisai.space"
 
 for instance in ${INSTANCES[@]}
 do
-   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro 
-   --security-group-ids sg-0ab70a84fcd87cc0d --tag-specifications "ResourceType=instance,
-   Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
-   if [$instance != "frontend"]
+   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-0ab70a84fcd87cc0d --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+      if [ $instance != "frontend" ]
    then
       IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
    else
