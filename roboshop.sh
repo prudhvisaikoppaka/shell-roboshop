9e2d023524fe9c7b62ad2ac17d26f2265ce73fe0
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-25 17:26:38 +0530

    roboshop documentation

--- roboshop.sh
+++ roboshop.sh
@@ -27,7 +27,7 @@ do
        "Changes": [{
        "Action"           : "UPSERT",
        "ResourceRecordSet": {
-         "Name"            : "'$DOMAIN_NAME'",
+         "Name"            : "'$RECORD_NAME'",
          "Type"            : "A",
          "TTL"             : 1,
          "ResourceRecords" : [{ 
