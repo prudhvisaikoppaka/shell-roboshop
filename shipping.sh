8ae6584a09f5faa58ee87470a2931d66b36d68d3
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-26 22:42:08 +0530

    roboshop project

--- shipping.sh
+++ shipping.sh
@@ -33,9 +33,9 @@ VALIDATE(){
 }
 
 dnf install maven -y &>>$LOG_FILE
-VALIDATE $? "Installing maven server"
+VALIDATE $? "Installing Maven and Java"
 
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
 then   
    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
@@ -48,29 +48,41 @@ mkdir -p /app
 VALIDATE $? "Creating app directory"
 
 curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading shipping user"
+VALIDATE $? "Downloading shipping"
 
+rm -rf /app/*
 cd /app 
 unzip /tmp/shipping.zip &>>$LOG_FILE
-VALIDATE $? "Unziping user"
+VALIDATE $? "Unziping shipping"
 
 mvn clean package &>>$LOG_FILE
-VALIDATE $? "Installing the clean package"
+VALIDATE $? "Packaging the shipping application"
 
 mv target/shipping-1.0.jar shipping.jar &>>$LOG_FILE
-VALIDATE $? "Building the application"
+VALIDATE $? "Moving and renaming Jar file"
 
 cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
 VALIDATE $? "Copying the shipping.service"
 
 systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "System reload"
+VALIDATE $? "Daemon Reload"
 
 systemctl enable shipping &>>$LOG_FILE
 VALIDATE $? "Enabling shipping"
 
 systemctl start shipping &>>$LOG_FILE
 VALIDATE $? "Starting shipping"
+
+dnf install mysql -y &>>$LOG_FILE
+VALIDATE $? "Install Client MySQL"
+
+mysql -h mysql.prudhvisai.space -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE 
+mysql -h mysql.prudhvisai.space -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
+mysql -h mysql.prudhvisai.space -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+VALIDATE $? "Loading data into MySQL"
+
+systemctl restart shipping &>>$LOG_FILE
+VALIDATE $? "Restart shipping"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME - $START_TIME))
