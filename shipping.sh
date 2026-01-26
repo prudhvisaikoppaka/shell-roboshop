cda081a598b82765175620ba2f68939206107710
Prudhvi Sai <prudhvisaikoppaka99@gmail.com>
2026-01-26 19:12:38 +0530

    roboshop documentation

--- shipping.sh
+++ shipping.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
+Y="\e[32m"
+G="\e[33m"
 N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
@@ -16,25 +16,56 @@ echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 if [ $USERID -ne 0 ]
 then
    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
-   exit 1 #give other than 0 upto 127
+   exit 1
 else
-   echo "You are running with root access" | tee -a $LOG_FILE
+   echo -e "$Y You are running this script with root access $N" | tee -a $LOG_FILE
 fi
 
-# Validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-  if [ $1 -eq 0 ]
-   then
-      echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
-   else
-      echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+VALIDATE if (){
+   if [ $1 -eq 0 ]
+    then
+      echo -e "$2 is ... $G Success $N" | tee -a $LOG_FILE
+    else
+      echo -e "$2 is ... $R Failure $N" | tee -a $LOG_FILE
       exit 1
-   fi       
+   fi    
 }
 
+dnf install maven -y
+VALIDATE $? "Installing maven server"
 
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "Creating roboshop system user"
+
+mkdir /app
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
+VALIDATE $? "Downloading shipping user"
+
+cd /app 
+unzip /tmp/shipping.zip
+VALIDATE $? "Unziping user"
+
+mvn clean package
+VALIDATE $? "Installing the clean package"
+
+mv target/shipping-1.0.jar shipping.jar
+VALIDATE $? "Building the application"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+VALIDATE $? "Copying the shipping.service"
+
+systemctl daemon-reload
+VALIDATE $? "System reload"
+
+systemctl enable shipping
+VALIDATE $? "Enabling shipping"
+
+systemctl start shipping
+VALIDATE $? "Starting shipping"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+TOTAL_TIME=$(($END_TIME - $START_TIME))
 
-echo -e "Script execution completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+echo -e "Script executation completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE  
